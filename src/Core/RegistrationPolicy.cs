eddd64d949d6544fa597b1ad747337a33a3097e7
Baiye <77656086+baiye-gift@users.noreply.github.com>
2026-09-17 22:17:07 +0800

    feat(phase2): separate phase registration groups

--- src/Core/RegistrationPolicy.cs
+++ src/Core/RegistrationPolicy.cs
@@ -3,22 +3,50 @@ using System.Collections.Generic;
 
 namespace ForbiddenTechnologyPack.Core {
     public sealed class RegistrationPlan {
+        private readonly string[] phase1BuildingIds;
+        private readonly string[] phase2BuildingIds;
         private readonly string[] buildingIds;
 
-        public RegistrationPlan(IEnumerable<string> buildingIds) {
-            if (buildingIds == null) {
-                throw new ArgumentNullException("buildingIds");
+        public RegistrationPlan(IEnumerable<string> phase1BuildingIds,
+                IEnumerable<string> phase2BuildingIds) {
+            if (phase1BuildingIds == null) {
+                throw new ArgumentNullException("phase1BuildingIds");
+            }
+            if (phase2BuildingIds == null) {
+                throw new ArgumentNullException("phase2BuildingIds");
             }
 
-            this.buildingIds = new List<string>(buildingIds).ToArray();
+            this.phase1BuildingIds = new List<string>(phase1BuildingIds).ToArray();
+            this.phase2BuildingIds = new List<string>(phase2BuildingIds).ToArray();
+
+            var all = new List<string>(this.phase1BuildingIds.Length + this.phase2BuildingIds.Length);
+            all.AddRange(this.phase1BuildingIds);
+            all.AddRange(this.phase2BuildingIds);
+            buildingIds = all.ToArray();
         }
 
         public IReadOnlyList<string> BuildingIds {
             get { return buildingIds; }
         }
 
+        public IReadOnlyList<string> Phase1BuildingIds {
+            get { return phase1BuildingIds; }
+        }
+
+        public IReadOnlyList<string> Phase2BuildingIds {
+            get { return phase2BuildingIds; }
+        }
+
         public bool HasAnyBuildings {
             get { return buildingIds.Length > 0; }
+        }
+
+        public bool HasPhase1Buildings {
+            get { return phase1BuildingIds.Length > 0; }
+        }
+
+        public bool HasPhase2Buildings {
+            get { return phase2BuildingIds.Length > 0; }
         }
 
         public bool IsEnabled(string buildingId) {
@@ -41,31 +69,33 @@ namespace ForbiddenTechnologyPack.Core {
                 throw new ArgumentNullException("options");
             }
 
-            var buildingIds = new List<string>();
+            var phase1BuildingIds = new List<string>();
+            var phase2BuildingIds = new List<string>();
             if (!options.ModuleEnabled) {
-                return new RegistrationPlan(buildingIds);
+                return new RegistrationPlan(phase1BuildingIds, phase2BuildingIds);
             }
 
             if (options.AnalyzerEnabled) {
-                buildingIds.Add(ModIdentity.MatterAnalyzerId);
+                phase1BuildingIds.Add(ModIdentity.MatterAnalyzerId);
             }
             if (options.CrusherEnabled) {
-                buildingIds.Add(ModIdentity.MassCrusherId);
+                phase1BuildingIds.Add(ModIdentity.MassCrusherId);
             }
             if (options.CompilerEnabled) {
-                buildingIds.Add(ModIdentity.MatterCompilerId);
-            }
-            if (options.ReconstructorEnabled) {
-                buildingIds.Add(ModIdentity.MatterReconstructorId);
-            }
-            if (options.EntropyDiverterEnabled) {
-                buildingIds.Add(ModIdentity.EntropyFluxDiverterId);
-            }
-            if (options.AnnihilationReactorEnabled) {
-                buildingIds.Add(ModIdentity.MatterAnnihilationReactorId);
+                phase1BuildingIds.Add(ModIdentity.MatterCompilerId);
             }
 
-            return new RegistrationPlan(buildingIds);
+            if (options.ReconstructorEnabled) {
+                phase2BuildingIds.Add(ModIdentity.MatterReconstructorId);
+            }
+            if (options.EntropyDiverterEnabled) {
+                phase2BuildingIds.Add(ModIdentity.EntropyFluxDiverterId);
+            }
+            if (options.AnnihilationReactorEnabled) {
+                phase2BuildingIds.Add(ModIdentity.MatterAnnihilationReactorId);
+            }
+
+            return new RegistrationPlan(phase1BuildingIds, phase2BuildingIds);
         }
     }
 }
